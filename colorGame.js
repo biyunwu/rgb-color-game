4b18900691f6e785757653dc56184f3eaad18ef6
BY <beenwu2016@gmail.com>
2018-04-08 22:55:56 -0400

    Refactor; add function to remove event listeners after the user chooses the right color block.

--- colorGame.js
+++ colorGame.js
@@ -10,34 +10,40 @@ function getRandomColors(){
 // Get an array of 6 random rgb colors.
 let colors = getRandomColors();
 
-// Assign colors to colorBlocks.
-let colorElements = document.querySelectorAll(".colorBlock");
-function initializeColors(){
-  for(let i=0; i<colorElements.length; i++){
-    colorElements[i].style.backgroundColor = colors[i];
-  }
-}
-
 // Choose a color's rgb value randomly and display in the h1 element.
 let colorSpan = document.getElementById("colorSpan");
 colorSpan.textContent = colors[Math.floor(Math.random()*colors.length)];
 
-// Initialize colors for colorBlocks when loading and refreshing.
-initializeColors();
-
-// Check correctness when colorBlocks are clicked.
+let colorElements = document.querySelectorAll(".colorBlock");
 let title = document.querySelector("h1");
-for (let i=0; i<colorElements.length; i++){
-  colorElements[i].addEventListener("click", function(){
-    // Get current colorBlock's background-clor value.
-    let curColor = window.getComputedStyle(this).getPropertyValue("background-color");
-    // Compare with rgb value in the title.
-    console.log(curColor);
-    let that = this;
-    if (curColor === colorSpan.textContent){
-      title.style.backgroundColor = curColor;
-    } else {
-      this.style.visibility = "hidden";
-    }
-  });
+let result = document.querySelector("#result");
+
+for(let i=0; i<colorElements.length; i++){
+  // Assign colors to colorBlocks.
+  colorElements[i].style.backgroundColor = colors[i];
+  // Check correctness and change background color when colorBlocks are clicked.
+  colorElements[i].addEventListener("click", actions);
+}
+
+function actions(){
+  let curColor = this.style.backgroundColor;
+  // Compare with rgb value in the title.
+  console.log(curColor);
+  let selectedColor = colorSpan.textContent;
+  if (curColor === selectedColor){
+    title.style.backgroundColor = curColor;
+    result.textContent = "Correct!"
+    // Change other colorBlocks' background color.
+    changeOthersColorAndRemoveEventListeners(selectedColor);
+  } else {
+    this.style.backgroundColor = "black";
+    result.textContent = "Try Again!"
+  }
+}
+
+function changeOthersColorAndRemoveEventListeners(rgbColorValue){
+  for(let i=0; i<colorElements.length; i++){
+    colorElements[i].style.backgroundColor = rgbColorValue;
+    colorElements[i].removeEventListener("click", actions);
+  }
 }
